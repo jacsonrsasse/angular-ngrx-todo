d3082579c64745341c506753b34efee6626d37e7
Jacson R. Sasse <jacson.sasse@hotmail.com>
2024-03-07 23:10:50 -0300

    Create a simple layout

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../states/task-list.state';
 import { CommonModule } from '@angular/common';
+import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TaskComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss',
 })
 export class TaskListComponent {
   @Input() tasks?: Task[];
+
+  trackByTaskId(index: number, task: Task) {
+    return index + task.id;
+  }
 }
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from '../states/task-list.state';
 
 @Component({
   selector: 'app-task',
   standalone: true,
   imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  styleUrl: './task.component.scss',
 })
 export class TaskComponent {
-
+  @Input() task!: Task;
 }
